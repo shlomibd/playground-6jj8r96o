4e1760d6fa8ffba092f2a0aa22a1ae5bd5734624
shlomibd <shlomibd@hotmail.com>
2024-03-07 10:20:38 +0200

    Update StockPriceTest.java

--- java-project/src/test/java/com/yourself/StockPriceTest.java
+++ java-project/src/test/java/com/yourself/StockPriceTest.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UniverseTest {
+public class StockPriceTest {
 
 	@Test
 	public void test() throws FileNotFoundException {
