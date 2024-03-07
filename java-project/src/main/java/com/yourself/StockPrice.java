76d69e1e1f5f4361ce04db01b03b85aa991ce497
shlomibd <shlomibd@hotmail.com>
2024-03-07 10:08:41 +0200

    Update and rename Universe.java to StockPrice.java

---
+++ java-project/src/main/java/com/yourself/StockPrice.java
@@ -0,0 +1,15 @@
+// { autofold
+package com.yourself;
+
+import java.util.Arrays;
+
+public class StockPrice {
+// }
+
+public static int getMaxProfit(int[] stockPricesYesterday) {
+	return -1;
+}
+
+//{ autofold
+}
+//}
